fc71e35ee05f7a740678213314ab156feab97084
SilvanHahn <silvan.hahn@tie-international.com>
2023-09-27 14:47:47 +0200

    Unnötige sachen

---
+++ Hauptseite/homepage.js
@@ -0,0 +1,5 @@
+const statisticsButton = document.getElementById("statisticsButton");
+statisticsButton.addEventListener("click", function () {
+    const statisticsPageUrl = "../Statistics/statistics.html";
+    window.location.href = statisticsPageUrl;
+});
