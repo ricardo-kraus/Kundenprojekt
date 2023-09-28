52a004aa3cd094d8ec8971ea43ddb966ad686794
Betty <142780441+Ellisabeta@users.noreply.github.com>
2023-09-28 09:06:31 +0200

    removed statistics page from hauptseite

--- Statistics/statistics.js
+++
@@ -1,5 +0,0 @@
-    const homepageButton = document.getElementById("homepageButton");
-    homepageButton.addEventListener("click", function () {
-        const homepageUrl = "../Hauptseite/homepage-modal-copy-2.html";
-        window.location.href = homepageUrl;
-    });
