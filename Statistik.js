886e3283f2f677c8c83e9918f56ec46aedb05031
SilvanHahn <silvan.hahn@tie-international.com>
2023-09-29 16:08:23 +0200

    .

--- Statistik.js
+++ Statistik.js
@@ -2,77 +2,106 @@ document.addEventListener("DOMContentLoaded", function () {
     // Daten aus dem Local Storage abrufen
     var gespeicherteDaten = localStorage.getItem("people");
     var datenArray = JSON.parse(gespeicherteDaten);
-
-    // Initialisieren Sie die Chart.js-Daten und Optionen
-    var chartData = {
-        labels: datenArray,
-        datasets: [
-            {
-                label: "positiv",
-                data: [], // Leer initialisieren
-                backgroundColor: "rgba(72, 168, 96, 0.4)",
-                borderColor: "green",
-                borderWidth: 1,
-            },
-            {
-                label: "negative",
-                data: [], // Leer initialisieren
-                backgroundColor: "rgba(198, 1, 31, 0.4)",
-                borderColor: "#9D2933",
-                borderWidth: 1,
-            },
-        ],
-    };
-
-    var options = {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-            y: {
-                beginAtZero: true,
-                ticks: {
-                    color: "black",
-                },
-            },
-            x: {
-                ticks: {
-                    color: "black",
-                },
-            },
+    Chart.defaults.color = 'black';
+    // Verwende die abgerufenen Daten als Labels
+    var data = {
+      labels: datenArray, // Hier werden die Namen aus den gespeicherten Daten verwendet
+      datasets: [
+        {
+          label: "positive",
+          data: [1, 2, 3, 4, 5], // Beispielwerte oder andere Daten
+          backgroundColor: "rgba(72, 168, 96, 0.4)",
+          borderColor: "green",
+          borderWidth: 1,
         },
+        {
+          label: "negative",
+          data: [1, 2, 3, 5, 6], // Beispielwerte oder andere Daten
+          backgroundColor: "rgba(198, 1, 31, 0.4)",
+          borderColor: "#9D2933",
+          borderWidth: 1,
+        },
+      ],
     };
-
-    // Das Diagramm initialisieren
-    var ctx = document.getElementById("mainDiagramm").getContext("2d");
-    var myChart = new Chart(ctx, {
-        type: "bar",
-        data: chartData,
-        options: options,
-    });
-
-    // Funktion zum Aktualisieren der Chart-Daten
-    function updateChartData() {
-        // Holen Sie die aktuellen Counts aus dem Local Storage
-        var updatedData = {
-            positive: [],
-            negative: [],
-        };
-
-        datenArray.forEach(function (name) {
-            var positiveCount = parseInt(localStorage.getItem(name + "_green_count")) || 0;
-            var negativeCount = parseInt(localStorage.getItem(name + "_red_count")) || 0;
-
-            updatedData.positive.push(positiveCount);
-            updatedData.negative.push(negativeCount);
-        });
-
-        // Aktualisieren Sie die Chart-Daten und zeichnen Sie das Diagramm neu
-        myChart.data.datasets[0].data = updatedData.positive;
-        myChart.data.datasets[1].data = updatedData.negative;
-        myChart.update();
-    }
-
-    // Initial die Chart-Daten aktualisieren
-    // Hier werden die Namen aus den gespeicherten Daten verwendet
-    updateChartData();
-});
+  
+      // Initialisieren Sie die Chart.js-Daten und Optionen
+      var chartData = {
+          labels: datenArray,
+          datasets: [
+              {
+                  label: "positive",
+                  data: [], // Leer initialisieren
+                  backgroundColor: "rgba(72, 168, 96, 0.4)",
+                  borderColor: "green",
+                  borderWidth: 1,
+              },
+              {
+                  label: "negative",
+                  data: [], // Leer initialisieren
+                  backgroundColor: "rgba(198, 1, 31, 0.4)",
+                  borderColor: "#9D2933",
+                  borderWidth: 1,
+              },
+          ],
+      };
+  
+      var options = {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+              y: {
+                  beginAtZero: true,
+                  ticks: {
+                      color: "white",
+                  },
+              },
+              x: {
+                  ticks: {
+                      color: "white",
+                  },
+              },
+          },
+      };
+  
+      // Das Diagramm initialisieren
+      var ctx = document.getElementById("mainDiagramm").getContext("2d");
+      var myChart = new Chart(ctx, {
+          type: "bar",
+          data: chartData,
+          options: options,
+      });
+  
+      // Funktion zum Aktualisieren der Chart-Daten
+      function updateChartData() {
+          // Holen Sie die aktuellen Counts aus dem Local Storage
+          var updatedData = {
+              positive: [],
+              negative: [],
+          };
+  
+          datenArray.forEach(function (name) {
+              var positiveCount = parseInt(localStorage.getItem(name + "_positive_count")) || 0;
+              var negativeCount = parseInt(localStorage.getItem(name + "_negative_count")) || 0;
+  
+              updatedData.positive.push(positiveCount);
+              updatedData.negative.push(negativeCount);
+          });
+  
+          // Aktualisieren Sie die Chart-Daten und zeichnen Sie das Diagramm neu
+          myChart.data.datasets[0].data = updatedData.positive;
+          myChart.data.datasets[1].data = updatedData.negative;
+          myChart.update();
+      }
+  
+      // Initial die Chart-Daten aktualisieren
+      // Hier werden die Namen aus den gespeicherten Daten verwendet
+      updateChartData();
+  });
+  
+  const adminpageButton = document.getElementById("adminpage-button");
+  
+  adminpageButton.addEventListener("click", function () {
+      const adminpageUrl = "./AdminPage.html";
+  
+      window.location.href = adminpageUrl;
+  });
--- test.js
+++ test.js
@@ -1,92 +1,170 @@
 // JavaScript-Funktionen
-
+let personName;
 function toggleDarkMode() {
-    const htmlTag = document.documentElement;
-    const darkmodeButton = document.getElementById("darkmode");
-  
-    if (htmlTag.getAttribute("data-bs-theme") === "dark") {
-      htmlTag.setAttribute("data-bs-theme", "light");
-      darkmodeButton.innerText = "Lightmode";
-      localStorage.setItem("mode", "light");
-    } else {
-      htmlTag.setAttribute("data-bs-theme", "dark");
-      darkmodeButton.innerText = "Darkmode";
-      localStorage.setItem("mode", "dark");
-    }
-  }
-  
-  const savedMode = localStorage.getItem("mode");
-  if (savedMode === "dark") {
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-    document.getElementById("darkmode").innerText = "Lightmode";
+  const htmlTag = document.documentElement;
+  const darkmodeButton = document.getElementById("darkmode");
+
+  if (htmlTag.getAttribute("data-bs-theme") === "dark") {
+    htmlTag.setAttribute("data-bs-theme", "light");
+    darkmodeButton.innerText = "Lightmode";
+    localStorage.setItem("mode", "light");
   } else {
-    document.documentElement.setAttribute("data-bs-theme", "light");
-    document.getElementById("darkmode").innerText = "Darkmode";
+    htmlTag.setAttribute("data-bs-theme", "dark");
+    darkmodeButton.innerText = "Darkmode";
+    localStorage.setItem("mode", "dark");
   }
-  
-  document.getElementById("darkmode").addEventListener("click", toggleDarkMode);
-  
-  // Weitere JavaScript-Funktionen hier einfügen
-  // Weitere JavaScript-Funktionen hier einfügen
-  
-  // Beispiel: Die Funktionen für das Generieren von Karten
-  assignmentsJSON = localStorage.getItem("assignments");
-  assignments = JSON.parse(assignmentsJSON);
-  
-  function generateCard(day, assignmentName, taskName, index) {
-    const cardId = `card${day}${index}`;
-    const card = document.createElement("div");
-    card.className = "card mx-auto mb-5 ";
-    card.innerHTML = `
-          <div class="card-body">
-              <div class="card-title fs-5 fw-semibold" id="${cardId}">
-                  ${assignmentName} <!-- Zuerst der Name -->
-              </div>
-              <div class="card-text" id="cardname${day}${index}">
-                  ${taskName} <!-- Dann die Aufgabe -->
-              </div>
-              <div>
-                  <button class="btn btn-success" id="greenButton${day}${index}" data-name="${assignmentName}">positive</button>
-                  <button class="btn btn-danger" id="redButton${day}${index}" data-name="${assignmentName}">negative</button>
-              </div>
-          </div>
-      `;
-  
-    document.getElementById(`card${day}`).appendChild(card);
-  
-    // Add event listeners for the green and red buttons
-    const greenButton = document.getElementById(`greenButton${day}${index}`);
-    const redButton = document.getElementById(`redButton${day}${index}`);
-  
-    greenButton.addEventListener("click", function () {
-      handleButtonSelection(this, "green");
-    });
-  
-    redButton.addEventListener("click", function () {
-      handleButtonSelection(this, "red");
-    });
-  }
-  
-  function handleButtonSelection(button, color) {
-    const assignmentName = button.getAttribute("data-name");
-    const storageKey = `${assignmentName}_${color}_count`;
-    let count = parseInt(localStorage.getItem(storageKey) || 0);
-    count++;
-    localStorage.setItem(storageKey, count);
-  }
-  
-  function generateCards(day, assignments) {
-    let index = 1;
-  
-    for (const assignmentName in assignments[day]) {
-      const taskName = assignments[day][assignmentName];
-      generateCard(day, assignmentName, taskName, index);
-      index++;
+}
+
+const savedMode = localStorage.getItem("mode");
+if (savedMode === "dark") {
+  document.documentElement.setAttribute("data-bs-theme", "dark");
+  document.getElementById("darkmode").innerText = "Lightmode";
+} else {
+  document.documentElement.setAttribute("data-bs-theme", "light");
+  document.getElementById("darkmode").innerText = "Darkmode";
+}
+
+document.getElementById("darkmode").addEventListener("click", toggleDarkMode);
+
+// Weitere JavaScript-Funktionen hier einfügen
+// Weitere JavaScript-Funktionen hier einfügen
+
+// Beispiel: Die Funktionen für das Generieren von Karten
+assignmentsJSON = localStorage.getItem("assignments");
+assignments = JSON.parse(assignmentsJSON);
+
+function generateCard(day, assignmentName, taskName, index) {
+  const cardId = `card${day}${index}`;
+  const card = document.createElement("div");
+  card.className = "card mx-auto mb-5 ";
+  card.innerHTML = `
+    <div class="card-body">
+      <div class="card-title fs-5 fw-semibold" id="${cardId}">
+        ${assignmentName} <!-- Zuerst der Name -->
+      </div>
+      <div class="card-text" id="cardname${day}${index}">
+        ${taskName} <!-- Dann die Aufgabe -->
+      </div>
+      <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+        <input type="radio" class="btn-check" name="rating-${day}-${index}" id="positive-${day}-${index}" autocomplete="off" value="positive" checked>
+        <label class="btn btn-outline-success" for="positive-${day}-${index}">Positive</label>
+
+        <input type="radio" class="btn-check" name="rating-${day}-${index}" id="negative-${day}-${index}" autocomplete="off" value="negative">
+        <label class="btn btn-outline-danger" for="negative-${day}-${index}">Negative</label>
+      </div>
+      <button class="btn btn-outline-light mt-2" data-bs-toggle="modal" data-bs-target="#commentModal-${day}-${index}">Comment</button>
+    </div>
+  `;
+
+  document.getElementById(`card${day}`).appendChild(card);
+
+  // Add event listeners for the green and red buttons
+  const positiveRadio = document.getElementById(`positive-${day}-${index}`);
+  const negativeRadio = document.getElementById(`negative-${day}-${index}`);
+
+  positiveRadio.addEventListener("change", function () {
+    handleRadioSelection(this, "positive");
+    personName = assignmentName;
+  });
+
+  negativeRadio.addEventListener("change", function () {
+    handleRadioSelection(this, "negative");
+    personName = assignmentName;
+  });
+
+  // Generate the modal for comments
+  generateCommentModal(day, index, assignmentName);
+}
+function generateCommentModal(day, index, personName) {
+  const modalId = `commentModal-${day}-${index}`;
+  const modal = document.createElement("div");
+  modal.className = "modal fade";
+  modal.id = modalId;
+  modal.innerHTML = `
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Comment for ${personName}</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
+        <div class="modal-body">
+          <textarea id="commentText-${day}-${index}" class="form-control" rows="5" placeholder="Enter your comment here"></textarea>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary" onclick="saveComment('${day}', ${index})">Save Comment</button>
+        </div>
+      </div>
+    </div>
+  `;
+
+  document.body.appendChild(modal);
+}
+
+// Function to save the comment
+function saveComment(day, index) {
+  const commentText = document.getElementById(`commentText-${day}-${index}`).value;
+  // You can save the comment in localStorage or perform other actions here
+  // For example: localStorage.setItem(`${personName}_${day}_comment`, commentText);
+}
+
+
+function handleRadioSelection(radio, color) {
+  if (radio.checked) {
+    const assignmentName = radio.getAttribute("name").split("-")[1];
+
+    const positiveStorageKey = `${personName}_${assignmentName}_positive_count`;
+    const negativeStorageKey = `${personName}_${assignmentName}_negative_count`;
+
+    // Check if the selected option is "positive" or "negative"
+    if (color === "positive") {
+      localStorage.setItem(positiveStorageKey, "1");
+      localStorage.setItem(negativeStorageKey, "0"); // Reset the negative count
+    } else if (color === "negative") {
+      localStorage.setItem(negativeStorageKey, "1");
+      localStorage.setItem(positiveStorageKey, "0"); // Reset the positive count
     }
+
+    // Update the card's data attributes to store the current selection
+    const cardId = `card${assignmentName}`;
+    const card = document.getElementById(cardId);
+    card.setAttribute(`data-${color}-count`, "1");
+    const oppositeColor = color === "positive" ? "negative" : "positive";
+    card.setAttribute(`data-${oppositeColor}-count`, "0");
+
+    // Calculate and store the total counts for the person
+    calculateAndStoreTotalCounts(personName, color);
   }
-  // Call the initial generation function AFTER it's defined
-  generateCards("monday", assignments);
-  generateCards("tuesday", assignments);
-  generateCards("wednesday", assignments);
-  generateCards("thursday", assignments);
-  generateCards("friday", assignments);
+}
+
+// Function to calculate and store total counts for a person
+function calculateAndStoreTotalCounts(personName, color) {
+  const days = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+  let totalCount = 0;
+
+  // Loop through each day and add up the counts
+  for (const day of days) {
+    const count = parseInt(localStorage.getItem(`${personName}_${day}_${color}_count`) || 0);
+    totalCount += count;
+  }
+
+  // Store the total count for the person
+  localStorage.setItem(`${personName}_${color}_count`, totalCount.toString());
+}
+
+function generateCards(day, assignments) {
+  let index = 1;
+
+  for (const assignmentName in assignments[day]) {
+    const taskName = assignments[day][assignmentName];
+    generateCard(day, assignmentName, taskName, index);
+    console.log(assignmentName);
+    index++;
+  }
+}
+// Call the initial generation function AFTER it's defined
+generateCards("monday", assignments);
+generateCards("tuesday", assignments);
+generateCards("wednesday", assignments);
+generateCards("thursday", assignments);
+generateCards("friday", assignments);
